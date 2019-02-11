22e9be3b3e9cae52d73315e50965ccc321fca851
misokim <misok1006@gmail.com>
2019-02-12 06:13:39 +0900

    add clock.js

---
+++ clock_test/clock.js
@@ -0,0 +1,20 @@
+console.log("hi!")
+
+const clockForm = document.querySelector(".js-clock");
+const clockArea = clockForm.querySelector("h1");
+
+
+function getTime() {
+  const date = new Date();
+  const hours = date.getHours();
+  const miniutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  clockArea.innerText = `${hours < 10 ? `0${hours}` : hours}:${miniutes < 10 ? `0${miniutes}` : miniutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000)
+}
+
+init();
